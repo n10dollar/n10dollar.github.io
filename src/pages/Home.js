fd6f35e111c2fd8d91a14d307792dc3568e86698
Neil <ntendolkar@berkeley.edu>
2023-10-27 20:07:20 -0700

    Tried out <Stack>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,32 @@
-import {Box} from "@mui/material";
+import {Box, Container, Paper, Stack, styled} from "@mui/material";
 import poker from "../assets/royal_flush.png";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 function Home() {
     return (
-        <Box
-            component={"img"}
-            sx={{
-                width: "100%",
-                height: "300px"
-            }}
-            src={poker}
-        / >
+        <Container>
+            <Box
+                component={"img"}
+                sx={{
+                    width: "100%",
+                    height: "300px"
+                }}
+                src={poker}
+            / >
+            <Stack>
+                <Item>This Could be you</Item>
+                <Item>This Could be you</Item>
+                <Item>This Could be you</Item>
+                <Item>This Could be you</Item>
+            </Stack>
+        </Container>
     )
 }
 
