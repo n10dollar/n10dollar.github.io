e1892168bcd9a6d1f56765349a656ba79e16943b
Neil <ntendolkar@berkeley.edu>
2023-10-27 19:39:45 -0700

    Created image <Box>

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {Container} from "@mui/material";
 function App() {
     let router = (
         <>
-            <NavBar />
+
             <BrowserRouter>
                 <Routes>
                     <Route path={'/'}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
-import {Container} from "@mui/material";
+import {Box} from "@mui/material";
 
 function Home() {
     return (
-        <Container>
-            My App
-        </Container>
+        <Box
+            component={"img"}
+            sx={{
+                width: "100%",
+                height: "300px"
+            }}
+            src={"https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/home-improvement/wp-content/uploads/2022/07/download-23.jpg"}
+        / >
     )
 }
 
