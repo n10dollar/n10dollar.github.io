cd53a5866d9b1ec40f95cf1e799cc55f3604c2da
Neil <ntendolkar@berkeley.edu>
2023-11-02 18:41:49 -0700

    Laid out <Grid> design and wrote intro description

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,19 +11,21 @@ import {
 } from "@mui/material";
 import {Image} from "mui-image";
 
+// model website: https://www.taniarascia.com/
 const listItems = ["A", "B", "C", "D"]
 
 function Home() {
     return (
         <Container>
-            <Grid container spacing={0}>
-                <Grid xs={12} sm={6}>
+            <Grid container spacing={2} align={"center"} py={2}>
+                <Grid item xs={12} sm={6} my={8}>
                     <Container style={{ background: '#ffff00' }}>
-                        <Typography>Hi!</Typography>
-                        <Typography>I'm Neil</Typography>
+                        <Typography>Hey, I'm Neil!</Typography>
+                        <Typography>Welcome to my digital studio.</Typography>
+                        <Typography>I'm a software developer in Berkeley. I make open-source projects and write about code, design, and life. I like producing music, tennis, cooking, reading, chess, and poker.</Typography>
                     </Container>
                 </Grid>
-                <Grid xs={12} sm={6} >
+                <Grid item xs={12} sm={6} >
                     <Image src={"/royal_flush.png"} />
                 </Grid>
             </Grid>
